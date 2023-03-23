b20a6fbc3674defbac530f9fa7040d4615726516
Douglas Flores <Dflores5df@icloud.com>
2023-03-22 20:26:39 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+multiply;
+
+const random = Math.floor(Math.random() * 10) + 1;
+random;
+
+const num3 = 8;
+const num4 = 20
+const mod = num4 % num3;
+mod;
+
+const max = Math.max(5, 10, 15, 20);
+max;
